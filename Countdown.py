35ef088077fb467cd49705055dbf3d23a48cf1aa
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-05-31 20:54:56 +0530

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -1,10 +1,12 @@
-﻿def countdown(a):
-	while True:
-		if a:
-			print a
-			a = a - 1
-			if a == 0:
+def countdown(a):                                    # Defining a procedure "Countdown" that takes single input
+	while True:                                  # Intialize While loop
+		if a:                                # If Statement
+			print a                      # Printing "a" will print value of "a" as is
+			a = a - 1                    # As we want to define countdown we must minus the initial value by 1
+			if a == 0:                   # when a's value becomes 0, then print "BlastOff!!"
 				print "Blastoff"
-				break
+				break                # breaking the loop
 				
 print countdown(7)
+
+# CODED BY - GAURAV PADAWE
